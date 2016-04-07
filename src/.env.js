36b9d0dfe7caee19b997ac28b223f971b9ff511f
Will Murray <willdavidmurray@gmail.com>
2016-04-07 17:16:10 +0100

    Move into config.js

--- src/.env.js
+++ src/.env.js
@@ -1,7 +1,5 @@
 export default {
   APP_KEY: 789,
   DEBUG: false,
-  HEARTBEAT_FREQUENCY: 5000,
-  COMPANY_NAME: 'ACME corp intl.',
-  SUPPORT_EMAIL: 'support@example.co.uk'
+  HEARTBEAT_FREQUENCY: 5000
 }
