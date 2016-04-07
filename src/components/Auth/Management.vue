c125110495c398db0be73d05611035a4a195a043
Will Murray <willdavidmurray@gmail.com>
2016-04-07 17:22:39 +0100

    Comments

--- src/components/Auth/Management.vue
+++ src/components/Auth/Management.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="col-sm-4 col-sm-offset-4 py2">
-    <p>Change user details etc.</p>
+
+    <p>Change your user profile details here.</p>
 
     <!--gravatar-->
     <gravatar-component
@@ -10,18 +11,23 @@
       :height="100"
       :width="100">
     </gravatar-component>
+
+    <!--is-this-you?-->
     <div v-show="user.email !== emailNew" class="col-1 m2 border border-silver rounded p1 mx-auto">
       <div v-show="user.email !== emailNew" class="bold blue" transition="bounce">Is this you?</div>
       <button @click="this.$emit('reset-email-field')" class="btn btn-outline border-silver rounded m1 red">&cross;</button>
       <button @click="this.$emit('apply-email-change')" class="btn btn-outline border-silver rounded m1 green">&check;</button>
     </div>
+
     <!--errors-->
     <div class="red" v-if="error">
       <p>{{ error }}</p>
     </div>
 
-    <!--profile info form container-->
+    <!--user profile info form container-->
     <div class="relative mx-auto">
+
+      <!--change email address-->
       <div class="form-group">
         <input
           type="text"
@@ -31,6 +37,8 @@
           debounce="800"/>
         <span v-show="emailCorrectlySet" class="bold green absolute top-0 right-0">&check;</span>
       </div>
+
+      <!--enter old password-->
       <div class="form-group">
         <input
           type="password"
@@ -38,6 +46,8 @@
           placeholder="Enter your old password"
           v-model="passwordOld" />
       </div>
+
+      <!--enter new password-->
       <div class="form-group">
         <input
           type="password"
@@ -45,6 +55,8 @@
           placeholder="Enter your new password"
           v-model="passwordNew" />
       </div>
+
+      <!--confirm password-->
       <div class="form-group">
         <input
           type="password"
@@ -52,7 +64,10 @@
           placeholder="Confirm your new password"
           v-model="passwordConfirm" />
       </div>
-      <button class="btn btn-primary m1" @click="this.$emit('login', credentials)">Update</button>
+
+      <!--update button-->
+      <button :class="{ 'muted': !canUpdate }" class="btn btn-primary m1" @click="this.$emit('login', credentials)">Update</button>
+
     </div>
   </div>
 </template>
@@ -72,7 +87,8 @@
         emailNew: '',
         passwordOld: '',
         passwordNew: '',
-        passwordConfirm: ''
+        passwordConfirm: '',
+        canUpdate: false
       }
     },
     ready () {
