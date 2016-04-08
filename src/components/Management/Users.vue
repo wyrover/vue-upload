483900200a21b7709250c2baf7f04186c20994a0
Will Murray <willdavidmurray@gmail.com>
2016-04-08 10:53:10 +0100

    Add

---
+++ src/components/Management/Users.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        types: ['identicon', 'mm', 'wavatar', 'retro', 'blank']
+      }
+    },
+    props: {
+      'users': { type: Object, required: true }
+    }
+  }
+</script>
